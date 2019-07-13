87d5ecf5fa1328a42f3b93448781e26d23a76c9a
alozs <alonso_freitas@hotmail.com>
2019-07-13 19:02:11 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+
+        <title>FSPHP TITULO</title>
+        <link rel="stylesheet" href="assets/style.css"
+    </head>
+    <body>
+ <?php
+$start = "vamos começar";
+echo "<h1>olá mundo! {$start}</h1>";
+echo "<p id='js'>Loading...</p>";
+    ?>  
+        <script src="assets/scripts.js"></script>
+</body>
+</html>
